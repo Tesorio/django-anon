2e7892d5e0ce507cd87ffa6eb10c671f6da21438
Caio Ariede <caio.ariede@gmail.com>
2019-10-21 10:19:37 -0300

    Add test scripts

---
+++ tests/settings.py
@@ -0,0 +1,5 @@
+INSTALLED_APPS = ["tests"]
+
+SECRET_KEY = "j^owl8=_)2do0don9sk@5k7obl!vxm!_404wf%yvk9rp3@a83#"
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
