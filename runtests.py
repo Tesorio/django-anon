7f51d7d74631b4bd6d215bc728824287fce9bd1f
Caio Ariede <caio.ariede@gmail.com>
2019-10-21 10:21:54 -0300

    Add missing file

---
+++ runtests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Recommended approach by Django:
+# https://docs.djangoproject.com/en/2.2/topics/testing/advanced/#using-the-django-test-runner-to-test-reusable-applications
+import os
+import sys
+import django
+
+if __name__ == "__main__":
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
+
+    from django.conf import settings
+    from django.test.utils import get_runner
+
+    django.setup()
+
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+    sys.exit(bool(failures))
