2cfca2cba46e17cfabe1bb2bc0a8dd61600cd483
Caio Ariede <caio.ariede@gmail.com>
2019-10-21 10:23:08 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.1"
+
+
+setup(
+    name="django-anon",
+    version=VERSION,
+    description="Anonymize production data so it can be safely used in not-so-safe environments",
+    author="Tesorio",
+    url="http://github.com/Tesorio/django-anon",
+    license="MIT",
+    zip_safe=False,
+    platforms=["any"],
+    packages=find_packages(),
+    package_data={},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    include_package_data=True,
+    install_requires=["django-bulk-update", "django-chunkator"],
+)
