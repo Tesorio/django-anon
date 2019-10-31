8dce9203bd62b9f1601ab7799a2dcaf4c8bf8cde
Caio Ariede <caio.ariede@gmail.com>
2019-10-31 16:40:06 -0300

    Update setup.py

--- anon/__init__.py
+++ anon/__init__.py
@@ -1,2 +1,11 @@
-from .base import BaseAnonymizer, lazy_attribute  # noqa: F401
-from .utils import *  # noqa: F401
+# noqa: F401
+import sys
+
+__version__ = "0.1"
+
+try:
+    from .base import BaseAnonymizer, lazy_attribute
+    from .utils import *
+except ImportError:
+    if not sys.argv[0].endswith('setup.py'):
+        raise
