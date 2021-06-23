6dfb7e54313eb571bbfb60b8998906c8872b940f
James Addison <james.addison@collabora.com>
2021-06-23 11:32:50 +0100

    Add illustrative test case

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -43,3 +43,7 @@ class UtilsTestCase(TestCase):
     def test_fake_phone_number(self):
         text = utils.fake_phone_number(format="(99) 9999-9999")
         self.assertTrue(bool(re.match(r"^\(\d{2}\) \d{4}-\d{4}$", text)))
+
+    def test_trim_text_empty_separator(self):
+        text = utils._trim_text(text="example", separator="", max_size=5)
+        self.assertTrue(len(text) <= 5)
