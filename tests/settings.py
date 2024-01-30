95539a4de16945dc9e763fdfeb24d11a524cf9d0
Hayden Richards <hayden.richards@3plearning.com>
2024-01-30 14:18:28 +0800

    Drop support for Python 2, Django 1.11 and 2.2. Add support for Python 3.7+, Django 3+.

--- tests/settings.py
+++ tests/settings.py
@@ -3,3 +3,5 @@ INSTALLED_APPS = ["tests"]
 SECRET_KEY = "j^owl8=_)2do0don9sk@5k7obl!vxm!_404wf%yvk9rp3@a83#"
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
